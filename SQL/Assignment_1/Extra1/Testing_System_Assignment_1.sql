da7c6a749aa1de0ef711866b22871ead9caa25fe
hoangnam13102000 <hoangnam131020@gmail.com>
2022-07-17 08:51:56 +0700

    submit assginment_1

---
+++ SQL/Assignment_1/Extra1/Testing_System_Assignment_1.sql
@@ -0,0 +1,16 @@
+create database QLthidau;
+create table Department(DepartmentID int primary key AUTO_INCREMENT, DepartmentName varchar(50));
+create table Posotion(PosotionID int, PosotionName ENUM('DEV','TEST','SCUM MASTER','PM'));
+create table Account(AccountID int primary key AUTO_INCREMENT, Email varchar(50), Username varchar(50), FullName varchar(50),DepartmentID int,PositionID int, CreateDate Date);
+create table Group1(GroupID int primary key AUTO_INCREMENT, GroupName varchar(50),CreatorID int,CreateDate date);
+create table GroupAccount(GroupID int, AccountID int,JoinDate date);
+create table TypeQuestion(TypeID int primary key AUTO_INCREMENT, TypeName varchar(50));
+create table CategoryQuestion(CategoryID int primary key AUTO_INCREMENT, CategoryName varchar(50));
+create table Question(QuestionID int primary key AUTO_INCREMENT, Content varchar(50),CategoryID int,TypeID int,CreatorID int, CreateDate date);
+create table Answer(AnswerID int primary key AUTO_INCREMENT, Content varchar(50),QuestionID int,isCorrect enum('true','false'));
+create table Exam(ExamID int primary key AUTO_INCREMENT, Code varchar(50),Title varchar(50),CategoryID int,Duration time, CreatorID int, CreateDate date);
+create table ExamQuestion(ExamID int, QuestionID int);
+
+
+
+
