e079bb35094c43bb011f2501b5b30c152d90e2dd
hoangnam13102000 <hoangnam131020@gmail.com>
2022-07-29 16:03:19 +0700

    Assignment4

--- SQL/Assignment_2/Extra_1/Testing_System_Assignment_2.sql
+++ SQL/Assignment_2/Extra_1/Testing_System_Assignment_2.sql
@@ -1,150 +1,113 @@
-drop database if exists QLthidau;
-create database if not exists QLthidau;
-use QLthidau;
-create table  Department(DepartmentID tinyint primary key AUTO_INCREMENT, 
-DepartmentName varchar(50) not null );
--- add data
-insert Department (DepartmentID,DepartmentName) values 
-(1,'Sale'),
-(2,'Marketing'),
-(3,'Nhân sự'),
-(4,'Bảo vệ'),
-(5,'Tài chính');
-create table Position (PositionID smallint primary key AUTO_INCREMENT, 
-PositionName ENUM('DEV','TEST','SCUM MASTER','PM'));
--- add data
-insert Position (PositionID,PositionName) values 
-(1,'DEV'),
-(2,'TEST'),
-(3,'TEST'),
-(4,'SCUM MASTER'),
-(5,'PM');
-create table Account(AccountID smallint primary key AUTO_INCREMENT, 
-Email  varchar(50) unique key, 
-Username varchar(50) unique key, 
-FullName varchar(50) Not NULL,
-DepartmentID tinyint not null,
-PositionID smallint not null, 
-CreateDate Date, 
-FOREIGN KEY (DepartmentID)
-REFERENCES Department (DepartmentID),
-FOREIGN KEY (PositionID)
-REFERENCES Position (PositionID)
+/*============================== CREATE DATABASE =======================================*/
+/*======================================================================================*/
+
+DROP DATABASE IF EXISTS TestingSystem;
+CREATE DATABASE TestingSystem;
+USE TestingSystem;
+
+/*============================== CREATE TABLE =======================================*/
+/*======================================================================================*/
+
+-- create table 1: Department
+CREATE TABLE Department(
+	DepartmentID 	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	DepartmentName 	VARCHAR(50) UNIQUE KEY NOT NULL
 );
--- add data
-insert Account (AccountID,Email,Username,FullName,DepartmentID,PositionID,CreateDate ) values 
-(1,'Dung1@.com.vn','Conchimto','Nguyen Van A',1,1,13/10/2000),
-(2,'Dung2@.com.vn','Conchimto2','Nguyen Van B',2,2,13/10/2000),
-(3,'Dung3@.com.vn','Conchimto3','Nguyen Van C',3,3,13/10/2000),
-(4,'Dung4@.com.vn','Conchimto4','Nguyen Van D',4,4,13/10/2000),
-(5,'Dung5@.com.vn','Conchimto5','Nguyen Van E',5,5,13/10/2000);
-create table Group1(GroupID int primary key AUTO_INCREMENT, 
-GroupName varchar(50),
-CreatorID tinyint not null unique,
-CreateDate date);
--- add data
-insert Group1 (GroupID ,GroupName,CreatorID,CreateDate) values 
-(1,'OOP',1,13/6/2001),
-(2,'TT',2,13/6/2001),
-(3,'TET',3,13/6/2001),
-(4,'MASTER',4,13/6/2001),
-(5,'POM',5,13/6/2001);
-create table GroupAccount(GroupID int not null,
- AccountID smallint not null unique,
- JoinDate date,
- FOREIGN KEY (AccountID)
-REFERENCES Account (AccountID),
-FOREIGN KEY (GroupID )
-REFERENCES Group1  (GroupID )
+
+-- create table 2: Position
+CREATE TABLE Position (
+	PositionID 		TINYINT UNSIGNED PRIMARY KEY,
+	PositionName 	ENUM('Sale', 'PM', 'Test', 'Vice Director', 'Scrum Master', 'Accountant', 'HR', 'Dev') UNIQUE KEY NOT NULL
 );
--- add data
-insert GroupAccount (GroupID ,AccountID,JoinDate) values 
-(1,1,13/6/2001),
-(2,2,13/6/2001),
-(3,3,13/6/2001),
-(4,4,13/6/2001),
-(5,5,13/6/2001);
-create table TypeQuestion(TypeID tinyint primary key AUTO_INCREMENT, 
-TypeName varchar(50));
--- add data
-insert TypeQuestion (TypeID,TypeName ) values 
-(1,'DEV'),
-(2,'TEST'),
-(3,'TEST'),
-(4,'SCUM MASTER'),
-(5,'PM');
-create table CategoryQuestion(CategoryID tinyint primary key AUTO_INCREMENT, 
-CategoryName varchar(50));
--- add data
-insert CategoryQuestion (CategoryID,CategoryName ) values 
-(1,'DEV'),
-(2,'TEST'),
-(3,'TEST'),
-(4,'SCUM MASTER'),
-(5,'PM');
-create table Question(QuestionID int primary key AUTO_INCREMENT, 
-Content varchar(50),
-CategoryID tinyint not null,
-TypeID tinyint not null,
-CreatorID tinyint not null, 
-CreateDate date,
-FOREIGN KEY (TypeID)
-REFERENCES TypeQuestion (TypeID),
-FOREIGN KEY (CategoryID)
-REFERENCES CategoryQuestion (CategoryID)
-);
--- add data
-insert Question (QuestionID ,Content,CategoryID,TypeID,CreatorID,CreateDate) values 
-(1,'OOP',1,1,1,13/6/2001),
-(2,'TT',2,2,2,13/6/2001),
-(3,'TET',3,3,3,13/6/2001),
-(4,'MASTER',4,4,4,13/6/2001),
-(5,'POM',5,5,5,13/6/2001);
-create table Answer(AnswerID int primary key AUTO_INCREMENT, 
-Content varchar(50),
-QuestionID int not null,
-isCorrect varchar(50),
-FOREIGN KEY (QuestionID)
-REFERENCES Question (QuestionID)
-);
--- add data
-insert Answer (AnswerID,Content,QuestionID,isCorrect ) values 
-(1,'DEV',1,'true'),
-(2,'TEST',2,'false'),
-(3,'TEST',3,'true'),
-(4,'SCUM MASTER',4,'false'),
-(5,'PM',5,'true');
-create table Exam(ExamID int primary key AUTO_INCREMENT, 
-Code varchar(50) unique,
-Title varchar(50),
-CategoryID tinyint not null,
-Duration int, 
-CreatorID tinyint not null, 
-CreateDate date,
-FOREIGN KEY (CategoryID)
-REFERENCES CategoryQuestion (CategoryID)
-);
--- add data
-insert Exam (ExamID ,Code,Title,CategoryID,Duration,CreatorID,CreateDate ) values 
-(1,'abc','DEV',1,1,1,13/6/2001),
-(2,'TEST','DEV',2,2,2,13/6/2001),
-(3,'T1ST','BA',3,3,3,13/6/2001),
-(4,'SCUM','BA',4,4,4,13/6/2001),
-(5,'PM','Maketing',5,5,5,13/6/2001);
-create table ExamQuestion(ExamID int not null, 
-QuestionID int not null,
-FOREIGN KEY (ExamID)
-REFERENCES Exam (ExamID),
-FOREIGN KEY (QuestionID)
-REFERENCES Question (QuestionID));
--- add data
-insert ExamQuestion (ExamID,QuestionID) values 
-(1,1),
-(2,2),
-(3,3),
-(4,4),
-(5,5);
 
 
+	-- create table 3: Account
+    CREATE TABLE `Account` (
+	AccountID 		SMALLINT UNSIGNED PRIMARY KEY,
+	Email 			VARCHAR(50) UNIQUE KEY NOT NULL,
+	User_name		VARCHAR(50) UNIQUE KEY NOT NULL CHECK (LENGTH(User_name) >= 6),
+	Full_name 		VARCHAR(50) NOT NULL CHECK (LENGTH(Full_name) >= 10),
+	DepartmentID	TINYINT UNSIGNED DEFAULT(1),
+	PositionID		TINYINT UNSIGNED DEFAULT(1),
+	CreateDate		DATETIME DEFAULT NOW(),
+    FOREIGN KEY (DepartmentID) REFERENCES Department (DepartmentID),
+	FOREIGN KEY (PositionID) REFERENCES Position (PositionID)
+);
+
+-- create table 4: Group
+CREATE TABLE `Group`(
+	GroupID 		SMALLINT UNSIGNED PRIMARY KEY,
+	GroupName 		VARCHAR(50) UNIQUE KEY NOT NULL,
+	CreatorID		SMALLINT UNSIGNED CHECK (CreatorID > 0 AND CreatorID < 8),
+	CreateDate		DATETIME,
+    FOREIGN KEY (CreatorID) REFERENCES `Account` (AccountID)
+);
+
+-- create table 5: GroupAccount
+CREATE TABLE GroupAccount (
+	GroupID 		SMALLINT UNSIGNED NOT NULL,
+	AccountID 		SMALLINT UNSIGNED NOT NULL CHECK (AccountID <8),
+	JoinDate		DATE NOT NULL,
+   PRIMARY KEY(GroupID, AccountID),
+   FOREIGN KEY (GroupID) REFERENCES `Group` (GroupID)
+    
+);
+
+-- create table 6: TypeQuestion
+CREATE TABLE TypeQuestion (
+	TypeID		SMALLINT UNSIGNED PRIMARY KEY,
+	TypeName	ENUM ('Essay', 'Multiple-Choice') UNIQUE KEY NOT NULL
+);
+
+-- create table 7: CategoryQuestion
+CREATE TABLE CategoryQuestion (
+	CategoryID		SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	CategoryName	VARCHAR(100) UNIQUE KEY NOT NULL
+);
+
+-- create table 8: Question
+CREATE TABLE Question (
+	QuestionID		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	Content			VARCHAR(100) UNIQUE KEY NOT NULL,
+	CategoryID		SMALLINT UNSIGNED NOT NULL,
+	TypeID			SMALLINT UNSIGNED NOT NULL,
+	CreatorID		SMALLINT UNSIGNED NOT NULL,
+	CreateDate		DATETIME DEFAULT NOW(),
+    FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID),
+    FOREIGN KEY (TypeID) REFERENCES TypeQuestion (TypeID),
+    FOREIGN KEY (CreatorID) REFERENCES `Account` (AccountID)
+    );
+
+-- create table 9: Answer
+CREATE TABLE Answer (
+	AnswerID 	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	Content		VARCHAR(100) NOT NULL,
+    QuestionID	TINYINT UNSIGNED NOT NULL,
+    isCorrect	ENUM ('đúng', 'sai') NOT NULL,
+    FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID)
+);
 
 
+-- create table 10: Exam
+CREATE TABLE Exam (
+	ExamID	 	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	`Code`		CHAR(10) UNIQUE KEY NOT NULL,
+    Title		VARCHAR(50) NOT NULL,
+    CategoryID	SMALLINT UNSIGNED ,
+    Duration	TINYINT UNSIGNED NOT NULL,
+    CreatorID	SMALLINT UNSIGNED NOT NULL,
+    CreateDate	DATE NOT NULL,
+    FOREIGN KEY (CreatorID) REFERENCES `Account` (AccountID),
+    FOREIGN KEY (CategoryID) REFERENCES `CategoryQuestion` (CategoryID)
+    
+);
+
+-- create table 11: ExamQuestion
+CREATE TABLE ExamQuestion (
+	ExamID	 	TINYINT UNSIGNED NOT NULL,
+	QuestionID	TINYINT UNSIGNED ,
+    FOREIGN KEY (ExamID) REFERENCES Exam (ExamID),
+	FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID),
+    PRIMARY KEY (ExamID,QuestionID)
+    
+);
